20802a56d64e4cded2d13b12ed800162d09005f1
Aleksandra8485 <118071817+Aleksandra8485@users.noreply.github.com>
2023-03-31 18:40:59 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const photoItem = document.createElement("li");
+  galleryEl.append(photoItem);
+
+  photoItem.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__link" href="${item.original}">
+  <img
+  class="gallery__image"
+  src="${item.preview}"
+  data-source="${item.original}"
+  alt="${item.description}"
+/>
+</a>`
+  );
+});
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
